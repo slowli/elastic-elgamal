8c7816f0dbc2f6080d7d9d8f13c4f270401ad55f
Alex Ostrovski <ostrovski.alex@gmail.com>
2023-07-29 13:32:50 +0300

    Disable new clippy lint

--- tests/integration/main.rs
+++ tests/integration/main.rs
@@ -1,5 +1,7 @@
 //! Generic tests independent of the `Group` implementation.
 
+#![allow(unknown_lints, clippy::items_after_test_module)] // Caused by `test_casing` macro
+
 mod basic;
 mod dkg;
 mod sharing;
