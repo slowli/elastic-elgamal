70d144c3b5a6d2bdd4c832aa5e2bbcd0299380d3
Alex Ostrovski <slowli@users.noreply.github.com>
2023-10-08 20:30:05 +0300

    Prepare 0.3.0 release (#48)
    ## What?
    
    Prepares the 0.3.0 crate release.
    
    ## Why?
    
    This will publish changes that were only partially available in the beta
    release.

--- src/lib.rs
+++ src/lib.rs
@@ -131,7 +131,7 @@
 #![cfg_attr(not(feature = "std"), no_std)]
 // Documentation settings.
 #![cfg_attr(docsrs, feature(doc_cfg))]
-#![doc(html_root_url = "https://docs.rs/elastic-elgamal/0.3.0-beta.1")]
+#![doc(html_root_url = "https://docs.rs/elastic-elgamal/0.3.0")]
 // Linter settings.
 #![warn(missing_debug_implementations, missing_docs, bare_trait_objects)]
 #![warn(clippy::all, clippy::pedantic)]
