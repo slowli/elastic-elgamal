e0eb4f8f513e6818c47d826e33d29b5597aa3a81
Alex Ostrovski <ostrovski.alex@gmail.com>
2025-11-08 15:45:55 +0200

    Add more instruction benchmarks

--- benches/instructions.rs
+++ benches/instructions.rs
@@ -22,20 +22,53 @@ impl BenchmarkedGroup for Generic<k256::Secp256k1> {
 }
 
 fn bench_group<G: BenchmarkedGroup>(bencher: &mut yab::Bencher) {
-    bencher.bench_with_capture(format!("{}/encrypt", G::NAME), |capture| {
+    let group = G::NAME;
+    bencher.bench_with_capture(format!("{group}/encrypt"), |capture| {
         let mut rng = ChaChaRng::from_seed([5; 32]);
         let keypair: Keypair<G> = Keypair::generate(&mut rng);
         let message = G::generate_scalar(&mut rng);
         capture.measure(|| keypair.public().encrypt(message, &mut rng));
     });
 
-    bencher.bench_with_capture(format!("{}/decrypt", G::NAME), |capture| {
+    bencher.bench_with_capture(format!("{group}/decrypt"), |capture| {
         let mut rng = ChaChaRng::from_seed([5; 32]);
         let keypair: Keypair<G> = Keypair::generate(&mut rng);
         let message = G::generate_scalar(&mut rng);
         let ciphertext = keypair.public().encrypt(message, &mut rng);
         capture.measure(|| keypair.secret().decrypt_to_element(ciphertext));
     });
+
+    bencher.bench_with_capture(format!("{group}/zero_prove"), |capture| {
+        let mut rng = ChaChaRng::from_seed([5; 32]);
+        let keypair: Keypair<G> = Keypair::generate(&mut rng);
+        capture.measure(|| keypair.public().encrypt_zero(&mut rng));
+    });
+
+    bencher.bench_with_capture(format!("{group}/zero_verify"), |capture| {
+        let mut rng = ChaChaRng::from_seed([5; 32]);
+        let keypair: Keypair<G> = Keypair::generate(&mut rng);
+        let (ciphertext, proof) = keypair.public().encrypt_zero(&mut rng);
+        capture
+            .measure(|| keypair.public().verify_zero(ciphertext, &proof))
+            .unwrap();
+    });
+
+    for val in [false, true] {
+        bencher.bench_with_capture(format!("{group}/bool_prove/{val}"), |capture| {
+            let mut rng = ChaChaRng::from_seed([5; 32]);
+            let keypair: Keypair<G> = Keypair::generate(&mut rng);
+            capture.measure(|| keypair.public().encrypt_bool(val, &mut rng));
+        });
+
+        bencher.bench_with_capture(format!("{group}/bool_verify/{val}"), |capture| {
+            let mut rng = ChaChaRng::from_seed([5; 32]);
+            let keypair: Keypair<G> = Keypair::generate(&mut rng);
+            let (ciphertext, proof) = keypair.public().encrypt_bool(val, &mut rng);
+            capture
+                .measure(|| keypair.public().verify_bool(ciphertext, &proof))
+                .unwrap();
+        });
+    }
 }
 
 fn benches(bencher: &mut yab::Bencher) {
